23d4ba64459b3daaca8dfb70ee1a457cc3b52110
Nagaraja Rao Gaddale <nagaraj@Nagarajas-MacBook-Air.local>
2025-06-19 12:20:27 +0200

    first commit

---
+++ tests/login.spec.js
@@ -0,0 +1,23 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pages/Login";
+
+test.describe("Login Functionality", () => {
+  let loginPage;
+
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    await loginPage.gotoLoginPage();
+  });
+
+  test("should successfully log in with valid credentials", async ({
+    page,
+  }) => {
+    const username = process.env.LINKEDIN_USERNAME;
+    const password = process.env.LINKEDIN_PASSWORD;
+    const loggedInUser = process.env.LINKEDIN_LOGGEDINUSER;
+    await loginPage.login(username, password);
+    await expect(await loginPage.isUserLoggedIn()).toBe(true);
+    await console.log(await loginPage.getLoggedInUsername());
+    await expect(await loginPage.getLoggedInUsername()).toBe(loggedInUser);
+  });
+});
